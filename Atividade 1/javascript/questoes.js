39115f29a17403b7e3ee8f973c10b5d91b8d39ae
Renner Borges <rennerferreira23@gmail.com>
2021-10-22 09:17:31 -0300

    terminando a questão 3

--- Atividade 1/javascript/questoes.js
+++ Atividade 1/javascript/questoes.js
@@ -1,14 +1,15 @@
 window.addEventListener('load', ()=> {
     FactoryQuestion1();
     FactoryQuestion2();
+    FactoryQuestion3();
 })
 
 function FactoryQuestion1(){
-    const button = document.querySelector('#questão1 > form button');
+    const form = document.querySelector('#questão1 > form');
     const input = document.querySelector('#questão1 > form input');
     const resultado = document.querySelector('#questão1 > form .resultado')
 
-    button.addEventListener('click', (event)=> {
+    form.addEventListener('submit', (event)=> {
         event.preventDefault();
         try {
             const factorial = calculateFactorial(input.value);
@@ -35,12 +36,12 @@ function calculateFactorial(number){
 }
 
 function FactoryQuestion2(){
-    const button = document.querySelector('#questão2 > form button');
+    const form = document.querySelector('#questão2 > form');
     const input = document.querySelector('#questão2 > form input');
     const resultado = document.querySelector('#questão2 .resultado');
     const errorMsg = document.querySelector('#questão2 .error_message');
 
-    button.addEventListener('click', (event)=> {
+    form.addEventListener('submit', (event)=> {
         event.preventDefault();
         try {
             const date = returnDateExtension(input.value);
@@ -76,4 +77,75 @@ function returnDateExtension(date){
     const [ano, mes, dia] = date.split('-');
 
     return `${dia} de ${meses[mes]} de ${ano}`;
+}
+
+function FactoryQuestion3(){
+    const form = document.querySelector('#questão3 > form');
+    const peso = document.querySelector('#questão3 > form input[name="peso"]');
+    const altura = document.querySelector('#questão3 > form input[name="altura"]');
+    const title = document.querySelector('#questão3 .content__info  h1');
+    const content = document.querySelector('#questão3 .content__info  h2');
+    const errorMsg = document.querySelector('#questão3 .error_message');
+
+    form.addEventListener('submit', (event)=> {
+        event.preventDefault();
+        try {
+            const imc = calculeImc(peso.value, altura.value);
+            const classificacao = classifyImc(imc);
+
+            console.log(title);
+            console.log(content);
+            title.innerHTML = `${classificacao.title}!!!`
+            content.innerHTML = `Seu IMC é ${imc.toFixed(2)} sua classificação é <b>${classificacao.content}</b>.`
+        } catch (error) {
+            
+        }
+    })
+}
+
+function calculeImc(peso, altura){
+    return peso/Math.pow(altura,2);
+};
+
+function classifyImc(imc){
+
+    const classificacoes = {
+        'abaixo_do_peso': {
+            condition: (imc)=> imc <= 18.5,
+            title: 'Atenção',
+            content: 'abaixo do peso',
+        },
+        'peso_ideal': {
+            condition: (imc)=> imc >= 18.6 && imc <= 24.9,
+            title: 'Parabéns',
+            content: 'peso ideal',
+        },
+        'acima_do_peso': {
+            condition: (imc)=> imc >= 25 && imc <= 29.9,
+            title: 'Cuidado',
+            content: 'levemente acima do peso',
+        },
+        'obesidade1': {
+            condition: (imc)=> imc >= 30 && imc <= 34.9,
+            title: 'Atenção',
+            content: 'obesidade grau I',
+        },
+        'obesidade2': {
+            condition: (imc)=> imc >= 35 && imc <= 39.9,
+            title: 'Atenção',
+            content: 'obesidade grau II',
+        },
+        'obesidade3': {
+            condition: (imc)=> imc >= 40,
+            title: 'Alerta',
+            content: 'obesidade grau III',
+        }
+    };
+
+    for(const classificacao of Object.values(classificacoes)){
+        if(classificacao.condition(imc)){
+            return classificacao;
+        }
+    }
+
 }
